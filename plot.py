827aa58d678b88b339f3d75f04218a418f11e24d
Oleksandr Klimenkov <alik.klimenkov@gmail.com>
2022-07-13 20:24:30 +0300

    plot with aligned x-axis

--- plot.py
+++ plot.py
@@ -1,40 +1,35 @@
-from distutils.command.config import config
-import numpy as np
 import matplotlib.pyplot as plt
-
+import numpy as np
+import os
 
 config_dict = {
     "sily.txt":  {4: "RX", 6: "RZ"},
     "perem.txt": {4: "VX"} }
 
+output_dir = "output"
+file_name = "perem.txt"
+file_path = os.path.join(output_dir, file_name)
 
-file_name = "sily.txt"
-data = np.loadtxt(file_name)
+data = np.loadtxt(file_path)
 columns_map_dict = config_dict[file_name]
+
 
 plt.suptitle(file_name)
 axes = []
 for i, (c, c_name) in enumerate(columns_map_dict.items(), 1):
-    axes.append(plt.subplot(len(columns_map_dict), 1, i))
-    plt.gca().set_aspect(aspect=30)
+    ax = plt.subplot(len(columns_map_dict), 1, i)
+    axes.append(ax)
+
     plt.grid()
 
     plt.xlabel("T")
     plt.ylabel(f"{c_name}")
-    # plt.gca().set_ylim(auto=True)
 
     plt.plot(data[:, 0], data[:, c])
 
+plt.subplots_adjust(hspace=0.5)
 plt.gca().get_shared_x_axes().join(*axes)
+
 plt.show()
-# plt.grid()
-# 
-
-
-# plt.xlabel("T")
-# plt.ylabel(f"{column_name}")
-
-# plt.plot(data[:, 0], data[:, 4])
-# # plt.plot(data[:, 0], data[:, 6])
 
 
